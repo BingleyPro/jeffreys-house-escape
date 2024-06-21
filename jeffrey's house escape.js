a78b8155409b33124fc7a72d4c2206b4dd24261b
Bingley <127451064+BingleyPro@users.noreply.github.com>
2024-06-21 18:30:51 +1000

    Started work on a progression mode

--- jeffrey's house escape.js
+++ jeffrey's house escape.js
@@ -1,10 +1,13 @@
 /*
-@title: jeffrey's house escape
+@title: Jeffrey's House Escape
 @author: bingleypro
-@tags: []
+@tags: [fun]
 @addedOn: 2024-06-19
-(the date above refers to the 19th of june)
+(the date above refers to the 19th of June, 2024)
 */
+
+// Use WASD or the left Sprig d-pad to move the player! Get the key and make it to the end.
+// For more information, check out https://github.com/BingleyPro/jeffreys-house-escape
 
 /* Define scene objects (these can be referenced in maps) */
 const player = "p"
@@ -12,11 +15,16 @@ const wall = "w"
 const door = "d"
 const key = "k"
 
-/* Change level size */
+const freeplayPortal = "f"
+const progressionPortal = "g"
+
+/* Default level size */
 const levelWidth = 10
 const levelHeight = 10
 
 var score = 0
+
+var mode = "freeplay"
 
 /* Setup sprites */
 setLegend(
@@ -54,7 +62,74 @@ L00L00L00L00L00L
 L0L00L00L00L0L0L
 LL00L00L00L000LL
 LLLLLLLLLLLLLLLL`],
-  [door, bitmap``],
+  [door, bitmap`
+................
+................
+....CCCCCCCC....
+....CCCCCCCC....
+...CCCCCCCCCC...
+...CCCCCCCCCC...
+..CCCCCCCCCCCC..
+..CCCCCCCCCCCC..
+..CCCCCCCCCCCC..
+..CCCCCCCCC6CC..
+..CCCCCCCC6C6C..
+..CCCCCCCCC6CC..
+..CCCCCCCCCCCC..
+..CCCCCCCCCCCC..
+..CCCCCCCCCCCC..
+..CCCCCCCCCCCC..`],
+  [key, bitmap`
+................
+.......66666....
+.......66666....
+.......66.......
+.......66.......
+.......6666.....
+.......66.......
+.......66.......
+.......66.......
+.....66666......
+....6666666.....
+....66...66.....
+....66...66.....
+....66...66.....
+....6666666.....
+.....66666......`],
+  [freeplayPortal, bitmap`
+....66666666....
+...6699999966...
+..669999999966..
+.66999999999966.
+6699999999999966
+6999999999999996
+6999999999999996
+6999999999999996
+6999999999999996
+6999999999999996
+6999999999999996
+6699999999999966
+.66999999999966.
+..669999999966..
+...6699999966...
+....66666666....`],
+  [progressionPortal, bitmap`
+....55555555....
+...5577777755...
+..557777777755..
+.55777777777755.
+5577777777777755
+5777777777777775
+5777777777777775
+5777777777777775
+5777777777777775
+5777777777777775
+5777777777777775
+5577777777777755
+.55777777777755.
+..557777777755..
+...5577777755...
+....55555555....`]
 )
 
 setSolids([player, wall])
@@ -63,51 +138,65 @@ setSolids([player, wall])
 let level = 0
 const levels = [
   map`
+fwwwg
+.www.
+..p..
+wwwww`, // Mode choosing
+  map`
 ...ww
 .w.ww
 ....k
 wwdwp`,
-/*  map`
-d...w.
-www...
-w....w
-wk....
-wpw...`,
+  /*  map`
+  d...w.
+  www...
+  w....w
+  wk....
+  wpw...`,
+    map`
+  ..w...
+  w.....
+  ..dwww
+  ..k...
+  ..p...
+  ..w.w.`,
+    map`
+  .......w.
+  pw..w...k
+  ....d...w
+  .........
+  ..w..w...
+  w..w.....`,
+    map`
+  ..........kw..
+  .......w....w.
+  .w............
+  ...w..........
+  ......p..w....
+  ..............
+  w.............
+  ..w...w.......
+  .w...........w
+  ....dw.....w..
+  w........w....
+  .w............`,
+    map`
+  ..w....
+  .....w.
+  ww.k...
+  p..d...
+  ...w...`,
+    map`
+  p.kdw`,*/
+]
+
+const progressionLevels = [
   map`
-..w...
-w.....
-..dwww
-..k...
-..p...
-..w.w.`,
-  map`
-.......w.
-pw..w...k
-....d...w
-.........
-..w..w...
-w..w.....`,
-  map`
-..........kw..
-.......w....w.
-.w............
-...w..........
-......p..w....
-..............
-w.............
-..w...w.......
-.w...........w
-....dw.....w..
-w........w....
-.w............`,
-  map`
-..w....
-.....w.
-ww.k...
-p..d...
-...w...`,
-  map`
-p.kdw`,*/
+.kwd.
+.www.
+.www.
+.www.
+..p..`
 ]
 
 let hasKey = false;
@@ -134,6 +223,7 @@ function movePlayer(direction) {
       newX += 1;
     }
 
+    // The player can get stopped by walls or window bounds
     if (newX < 0 || newY < 0 || newX >= width() || newY >= height() || getTile(newX, newY).some(sprite => sprite.type === "w")) {
       moved = false;
     } else {
@@ -159,19 +249,53 @@ onInput("d", () => movePlayer("right"));
 
 /* After completion, generate a new level */
 afterInput(() => {
-  if (tilesWith(door, player).length > 0 && hasKey) {
-    level += 1;
-    score += 1;
+  if (mode == "freeplay") {
+    if (tilesWith(door, player).length > 0 && hasKey) {
+      level += 1;
+      score += 1;
+  
+      addText("Score:" + score, {
+        x: 10,
+        y: 4,
+        color: color`3`
+      })
+  
+      // Generate a level!
+      generateNewLevel()
+  
+      const currentLevel = levels[level];
+      hasKey = false;
+  
+      if (currentLevel !== undefined) {
+        setMap(currentLevel);
+      } else {
+        addText("you win!", { y: 4, color: color`3` });
+      }
+    }
+  } else if (mode == "progression") {
+    if (tilesWith(keys) == keys && tilesWith(door, player).length > 0) {
+      keys = 0
+      
+      level += 1;
+      score += 1;
 
-    addText("Score:" + score, {
-      x: 10,
-      y: 4,
-      color: color`3`
-    })
+      const currentLevel = progressionLevels[level];
 
-    // Generate a level!
-    generateNewLevel()
+      if (currentLevel !== undefined) {
+        setMap(currentLevel);
+      } else {
+        addText("you win!", { y: 4, color: color`3` });
+      }
+    }
+  }
+
+  if (tilesWith(freeplayPortal, player).length > 0) {
+    // Freeplay mode selected
+    mode = "freeplay"
     
+    level += 1;
+    generateNewLevel()
+
     const currentLevel = levels[level];
     hasKey = false;
 
@@ -180,6 +304,15 @@ afterInput(() => {
     } else {
       addText("you win!", { y: 4, color: color`3` });
     }
+    
+  } else if (tilesWith(progressionPortal, player).length > 0) {
+    // Progression mode selected
+    mode = "progression"
+
+    var keys = 0
+    
+    const currentLevel = progressionLevels[level];
+    setMap(currentLevel);
   }
 });
 
@@ -191,9 +324,9 @@ function bfs(start, end, grid) {
 
   const directions = [
     { x: 0, y: -1 }, // up
-    { x: 0, y: 1 },  // down
+    { x: 0, y: 1 }, // down
     { x: -1, y: 0 }, // left
-    { x: 1, y: 0 }   // right
+    { x: 1, y: 0 } // right
   ];
 
   while (queue.length > 0) {
@@ -207,7 +340,7 @@ function bfs(start, end, grid) {
     for (let dir of directions) {
       let newX = x;
       let newY = y;
-      
+
       while (
         newX + dir.x >= 0 && newX + dir.x < grid[0].length &&
         newY + dir.y >= 0 && newY + dir.y < grid.length &&
@@ -216,7 +349,7 @@ function bfs(start, end, grid) {
         newX += dir.x;
         newY += dir.y;
       }
-      
+
       // Check if the new position is visited
       if (!visited.has(`${newX},${newY}`)) {
         queue.push({ x: newX, y: newY });
@@ -293,13 +426,11 @@ function generateLevel(width, height) {
 
 function generateNewLevel() {
   // Generate a level!
-let newLevel;
-do {
-  newLevel = generateLevel(levelHeight, levelWidth);  // or any other size
-} while (!isLevelSolvable(newLevel.split("\n").map(row => row.split(''))));
+  let newLevel;
+  do {
+    newLevel = generateLevel(levelHeight, levelWidth); // or any other size
+  } while (!isLevelSolvable(newLevel.split("\n").map(row => row.split(''))));
 
-levels.push(map`${newLevel}`);
-setMap(levels[level]);
+  levels.push(map`${newLevel}`);
+  setMap(levels[level]);
 }
- 
-generateNewLevel()
